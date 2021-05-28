591e35c9c8e1a8b5b2b4a3cda706381e279af6ec
akaPython <thebluethunder31@gmail.com>
2021-05-28 16:50:57 +0200

    added flipping animation

--- js/animatingcard.js
+++ js/animatingcard.js
@@ -1,38 +1,56 @@
-$().ready(flipcards)
-setInterval(flipcards, 950)
-function flipcards(){
-    var secondCheck = increaseseconds()
+setInterval(decreaseSeconds, 990)
+function decreaseSeconds(){
+    var changeBackText = $(".card-back-text")[3].innerText
+    if(changeBackText==0){
+        decreaseMinutes()
+        changeBackText = 60
+    }
+    $(".card-back-text")[3].innerText = changeBackText-1
+    var addAnimationTop = $(".card-top")[3].classList
+    var addAnimationBottom = $(".card-bottom")[3].classList
+    addAnimationTop.add("animating-top")
+    setTimeout(function(){
+        addAnimationBottom.add("animating-bottom")
+        $(".animating-top > .seconds").text(changeBackText-1)
+        $(".animating-bottom > .seconds").text(changeBackText-1)
+    },400)
+    setTimeout(function(){
+        
+        addAnimationBottom.remove("animating-bottom")
+        addAnimationTop.remove("animating-top")
+    },900)
+}
+
+function decreaseMinutes(){
+    var changeBackText = $(".card-back-text")[2].innerText
+    if(changeBackText==0){
+        decreaseHours()
+        changeBackText = 60
+    }
+    $(".card-back-text")[2].innerText = changeBackText-1
+    var addAnimationTop = $(".card-top")[2].classList
+    var addAnimationBottom = $(".card-bottom")[2].classList
+    addAnimationTop.add("animating-top")
+    setTimeout(function(){
+        addAnimationBottom.add("animating-bottom")
+        $(".animating-top > .minutes").text(changeBackText-1)
+        $(".animating-bottom > .minutes").text(changeBackText-1)
+    },400)
+    setTimeout(function(){
+        
+        addAnimationBottom.remove("animating-bottom")
+        addAnimationTop.remove("animating-top")
+    },900)
     
 }
-function increaseseconds(){
 
-    var toCheck = Number($(".seconds")[0].innerText)
-    console.log(typeof(toCheck))
-    if(toCheck==0){
-        $(".seconds").text(60)
-        increaseMinutes();
-    }else{
-        $(".seconds").text(toCheck-=1)
-    }
-}
-function increaseMinutes(){
-    var toCheck = Number($(".minutes")[0].innerText)
-    console.log(typeof(toCheck))
-    if(toCheck==0){
-        $(".minutes").text(60)
-        increaseHours();
-    }else{
-        $(".minutes").text(toCheck-=1)
-    }
-}
-
-function increaseHours(){
-    var toCheck = Number($(".hours")[0].innerText)
-    console.log(typeof(toCheck))
-    if(toCheck==0){
-        $(".hours").text(24)
-        increasedays();
-    }else{
-        $(".hours").text(toCheck-=1)
-    }
-}
+// function increaseHours(){
+//     var toCheck = Number($(".hours")[0].innerText)
+//     console.log(typeof(toCheck))
+//     if(toCheck==0){
+//         $(".hours").text(24)
+//         increasedays();
+//     }else{
+//         $(".hours").text(toCheck-=1)
+//     }
+// }
